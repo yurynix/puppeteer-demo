0ac0f81758ecc1f5182055836bb7eea52fbe4287
Yury Michurin <yurym@wix.com>
2019-06-23 08:47:37 +0300

    Remove junk

--- __tests__/wix.spec.js
+++ __tests__/wix.spec.js
@@ -1,13 +1,8 @@
 describe('Wix', () => {
     beforeAll(async () => {
-      console.log('HEADLESS', process.env.HEADLESS);
       await page.goto('https://www.wix.com')
     });
   
-    // it('should display "wix" text on page', async () => {
-    //   await expect(page).toMatch('wix')
-    // });
-
     it('should click "Sign In"', async () => {
       await page.evaluate(() => {
         const el = Array.from(document.querySelectorAll('span')).find(el => el.innerText.includes('Sign In'))
